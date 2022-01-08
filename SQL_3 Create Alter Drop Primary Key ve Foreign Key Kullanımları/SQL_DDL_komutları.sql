58fb7171179227f3ccdd3b8b879a6ccd1a374cd0
ibrahim-ethem-ispir <79301986+ibrahim-ethem-ispir@users.noreply.github.com>
2022-01-08 22:02:09 +0300

    Add files via upload

---
+++ SQL_3 Create Alter Drop Primary Key ve Foreign Key Kullanımları/SQL_DDL_komutları.sql
@@ -0,0 +1,67 @@
+
+/* CREATE -> Veritabanýnda nesne oluþturmak için kullanýlýr */
+/* Drop -> Veritabanýnda bulunan Bir nesneyi silmek / yok etmek için kullanýlýr */
+/* ALTER -> Veritabanýnda deðiþiklik yapma iþlemlerinde kullanýlýr */
+
+
+/* 1- Veritabaný Oluþturun ve bu veritabanýnda çalýþmak için gereken kodu yazýn */
+create database test
+
+/* 2- Oluþturmuþ Olduðunuz Veritabanýný Silin */
+
+
+/* 3- Veritabaný Oluþturun ve içerisine öðrenci adýnda 1 tablo oluþturun 
+	ogrno 5 karakter metin tipinde, 
+	adi 15 karakter metin tipinde,
+	soyadi 5 karakter metin tipinde,
+	dogum_tarih sadece yýl-ay-gün içeren tipte,
+	bolum_id 0 ile 255 arasý sayýlarý alabilsin
+		alanlarýna sahip olsun */
+
+
+/* 4- Veritabanýna bolum adýnda tablo oluþturun bolum_id tinyint tipinde ve boþ geçilemesin, 
+	aciklama alaný metin tipinde 70 karakterden oluþsun ve boþ geçilebilsin alanlarý olsun  */
+
+
+/* 5- Veritabanýna bolum1 adýnda tablo oluþturun bolum_id 0-255 arasý olsun sayý tipinde, aciklama
+	metin titpinde 70 karakter . Alanlarýný oluþturun. 
+	ve bolum_id primary key yapýn */
+
+
+/* 6- bolum 1 tablosunu silin */
+
+
+/* 7- Öðrenci tablosuna tel alaný ekle 11 karakter metin titpinde olsun */
+
+
+/* 8- Öðrenci tablosuna adres alaný ekle 50 karakter metin titpinde olsun */
+
+
+/* 9- Öðrenci tablosuna adres2 alaný ekle 50 karakter metin titpinde olsun */
+
+
+/* 10- Öðrenci tablosuna adres2 alanýný silin */
+
+
+/* 11- Öðrenci tablosuna adres alanýný metin tipinde 60 karakter olarak güncelleyin */
+
+
+/* 12- Öðrenci tablosuna ogrno alanýný birincil anahtar yapýnýz */
+
+
+/* 13- Öðrenci tablosuna ogrno alanýný birincil anahtarý silin */
+
+
+/* 14- bölüm tablosunda bolum_id alanýný birincil anahtar yapýnýz */
+
+
+/* 15- ogrenci tablosunun bolum_id alanýnýna yabancý anahtar kýsýtlamasý ekleyiniz  
+	referansý bolum(bolum_id) olacak þekilde */
+
+
+/* 16- bölüm tablosuna bölümler ekleyiniz */
+
+/* 17- öðrenci tablosunu öðrenci ekleyin ve foreign key baðlantýsýný 
+	olmayan bir bölüm girerek kontrol edin */
+
+
